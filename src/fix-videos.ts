6da90b88d4a78bcf07c0ddc8d1b70dfdebeb74f7
Ralph Wang <traveller@163.com>
2024-02-23 17:52:03 +0800

    feat: 修正 video 元素的 controls 和 track 元素的 default 属性

--- src/fix-videos.ts
+++ src/fix-videos.ts
@@ -21,32 +21,37 @@ function* walk(dir: string): Generator<string> {
   }
 }
 
-async function fixVideos() {
-  owners.forEach(owner => {
-    subNames.forEach(subName => {
+function fixVideos() {
+  for (const owner of owners) {
+    for (const subName of subNames) {
       const files = walk(`output/github/${owner}/${subName}angular.cn/generated/docs`);
       for (const file of files) {
+        console.log('fix video:', file);
         if (!file.endsWith('README.json') && file.endsWith('.json')) {
           const json = JSON.parse(readFileSync(file, 'utf-8'));
           const contents = json.contents as string;
           if (contents?.match(/\/?assets\/videos\//)) {
             json.contents = contents
                 .replace(/<div class="error-video-container">\s+<iframe src="\/?assets\/videos\/([^"]*)(.cn)?\.mp4" .*?><\/iframe>\s+<\/div>/, `<div class="video-container">
-  <video controls="">
+  <video controls>
   <source src="https://videos.angular.cn/$1.webm" type="video/webm">
   <source src="https://videos.angular.cn/$1.mp4" type="video/mp4">
   <track src="https://videos.angular.cn/$1.en.vtt" label="English" kind="subtitles" srclang="en">
-  <track src="https://videos.angular.cn/$1.cn.vtt" label="简体中文" kind="subtitles" srclang="zh-CN" default="">
+  <track src="https://videos.angular.cn/$1.cn.vtt" label="简体中文" kind="subtitles" srclang="zh-CN" default>
   <track src="https://videos.angular.cn/$1.tw.vtt" label="正體中文" kind="subtitles" srclang="zh-TW">
 </video>`)
                 .replace(/src="\/?assets\/videos\/([^.]*?).(webm|mp4|en.vtt|cn.vtt|tw.vtt)"/g, 'src="https://videos.angular.cn/$1.$2"');
-            writeFileSync(file, JSON.stringify(json, null, 2), 'utf8');
           }
+          if (contents?.match(/<video controls="">/)) {
+            json.contents = contents
+                .replace(/<video controls="">/g, '<video controls>')
+                .replace(/srclang="zh-CN" default="">/g, 'srclang="zh-CN" default>');
+          }
+          writeFileSync(file, JSON.stringify(json), 'utf8');
         }
       }
-    });
-  });
+    }
+  }
 }
 
-
-fixVideos().then(console.log);
+fixVideos();
